76ec3fb155bf99e9bfa9e93811bb6eff96c1453a
duddk <duddk@scatterx.io>
2024-08-16 16:43:40 +0900

    create 기능 추가

--- src/components/TodoCreate.tsx
+++ src/components/TodoCreate.tsx
@@ -1,23 +1,57 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { MdAdd } from 'react-icons/md';
-import { CircleButton, InsertFormPositioner, InsertForm, Input } from './styles.css';
+import {
+  CircleButton,
+  InsertFormPositioner,
+  InsertForm,
+  Input,
+} from './styles.css';
+import { useTodoDispatch, useTodoNextId } from './TodoContext';
 
-export default function TodoCreate(){
+function TodoCreate() {
   const [open, setOpen] = useState(false);
-  const onToggle = () => setOpen(!open);
+  const [value, setValue] = useState('');
 
-  return(
+  const dispatch = useTodoDispatch();
+  const nextId = useTodoNextId();
+
+  const onToggle = () => setOpen(!open);
+  const onChange = (e) => setValue(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault(); //새로고침 방지
+
+    dispatch({
+      type: 'CREATE',
+      todo: {
+        id: nextId.current,
+        text: value,
+        done: false,
+      },
+    });
+    setValue('');
+    setOpen(false);
+    nextId.current += 1;
+  };
+
+  return (
     <>
       {open && (
         <div className={InsertFormPositioner}>
-          <InsertForm>
-            <Input autoFocus placeholder='할 일을 입력 후, Enter를 누르세요.'></Input>
+          <InsertForm onSubmit={onSubmit}>
+            <Input
+              autoFocus
+              placeholder="할 일을 입력 후, Enter를 누르세요."
+              onChange={onChange}
+              value={value}
+            ></Input>
           </InsertForm>
         </div>
       )}
-      <button className={CircleButton({open})} onClick={onToggle}>
-        <MdAdd/>
+      <button className={CircleButton({ open })} onClick={onToggle}>
+        <MdAdd />
       </button>
     </>
-  )
+  );
 }
+
+export default React.memo(TodoCreate);
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,24 +1,30 @@
+import React from 'react';
 import { MdDone, MdDelete } from 'react-icons/md';
 import { TodoItemBlock, CheckCircle, Text, Remove } from './styles.css';
 import { useTodoDispatch } from './TodoContext';
 
 type CompProps = {
-  id: number
+  id: number;
   text: string;
   done: boolean;
-}
+};
 
-export default function TodoItem({id, text, done}: CompProps){
+function TodoItem({ id, text, done }: CompProps) {
   const dispatch = useTodoDispatch();
-  const onToggle = () => dispatch({ type: 'TOGGLE', id })
-  const onRemove = () => dispatch({ type: 'REMOVE', id })
-  
+  const onToggle = () => dispatch({ type: 'TOGGLE', id });
+  const onRemove = () => dispatch({ type: 'REMOVE', id });
+
   return (
     <div className={TodoItemBlock}>
-      <div className={CheckCircle({done})} onClick={onToggle}>{done && <MdDone/>}</div>
-      <div className={Text({done})}>{text}</div>
-      <div className={Remove} onClick={onRemove}><MdDelete/></div>
+      <div className={CheckCircle({ done })} onClick={onToggle}>
+        {done && <MdDone />}
+      </div>
+      <div className={Text({ done })}>{text}</div>
+      <div className={Remove} onClick={onRemove}>
+        <MdDelete />
+      </div>
     </div>
-  )
+  );
 }
 
+export default React.memo(TodoItem);
